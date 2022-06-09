80ca6e33b0acad63cc0a3b79a689290de1257817
LauraAdom <laura.adomaityt@gmail.com>
2022-06-09 09:36:53 +0200

    models, authent and autho

---
+++ src/main/java/com/ironhack/cyclobuddyserver/CycloBuddyServerApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.cyclobuddyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CycloBuddyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CycloBuddyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/cyclobuddyserver/CycloBuddyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.cyclobuddyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CycloBuddyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
